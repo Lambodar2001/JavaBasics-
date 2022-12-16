787d827f366d1efcf33b2778c89246da88f68f8b
Lambodar <lambodarwaghmare2001@gmail.com>
2022-12-16 22:36:41 +0530

    prog1- If else

---
+++ first.java
@@ -0,0 +1,19 @@
+public class first{
+    public static void main(String args[]){
+
+        boolean carryUmbrella;
+        boolean rain = true ;
+
+        if(rain==true){
+            carryUmbrella= true;
+
+        }
+
+        else{
+            carryUmbrella= false;
+
+        }
+
+        System.out.println(carryUmbrella);
+    }
+}
